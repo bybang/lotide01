c1da1688d153c161d9e883280484c2c9dfe93068
bybang <bybang26@gmail.com>
2021-12-01 15:51:54 -0500

    first commit for countLetters function

---
+++ countLetters.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// countLetters Function
+const countLetters = () => {
+  
+}
