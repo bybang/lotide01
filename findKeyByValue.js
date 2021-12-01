aef558f7fe8eb11568255d2c4354ac25be17251d
bybang <bybang26@gmail.com>
2021-12-01 17:15:01 -0500

    first commit for findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Find Key by Value function
+const findKeyByValue = () => {
+  
+}
